f93d4bc8a3184b3c6de00434cf7ae650322ffcfd
Jorge Leon <jorgeleonpicture@gmail.com>
2022-07-21 23:03:42 +0200

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1 className={styles.locura}>HOLA MUNDO!</h1>
-      <h1>QUE LOCURA</h1>
+      <Home />
+      <Footer />
     </div>
   );
 }
