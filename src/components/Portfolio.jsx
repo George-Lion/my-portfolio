40e682035dfe38cd40a19df1d63237686d55160d
Jorge Leon <jorgeleonpicture@gmail.com>
2022-07-26 16:37:33 +0200

    portfolio: cards

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,161 +7,180 @@ const Portfolio = () => {
   let url4 = "https://traveland-v2.herokuapp.com/";
   return (
     <Fragment>
-      <div className={styles.portfolio} id="portfolio">
-        <div>
-          <p className={styles.portfolio__title}>- Portfolio</p>
-        </div>
-
-        <div
-          className={styles.portfolio__services}
-          data-aos="fade-up"
-          data-aos-anchor-placement="center-bottom"
-        >
-          <div className={styles.portfolio__services__card}>
-            <div className={styles.portfolio__services__card__head}>
-              <a
-                type="button"
-                href={url4}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  className={styles.portfolio__services__card__head}
-                  src={img1}
-                />
-              </a>
-            </div>
-            <div className={styles.portfolio__services__card__down}>
-              <h3 className={styles.portfolio__services__card__down__title}>
-                Traveland web app.
-              </h3>
-              <div className={styles.portfolio__services__card__down__tecno}>
-                <h5
-                  className={
-                    styles.portfolio__services__card__down__tecno__code
-                  }
-                >
-                  JavaScript
-                </h5>
-                <h5
-                  className={
-                    styles.portfolio__services__card__down__tecno__code
-                  }
-                >
-                  React
-                </h5>
-                <h5
-                  className={
-                    styles.portfolio__services__card__down__tecno__code
-                  }
-                >
-                  CSS
-                </h5>
-                <h5
-                  className={
-                    styles.portfolio__services__card__down__tecno__code
-                  }
-                >
-                  Python
-                </h5>
-              </div>
-              <div className={styles.portfolio__services__card__down__links}>
-                <a
-                  className={
-                    styles.portfolio__services__card__down__links__link
-                  }
-                  href="https://github.com/George-Lion/FINAL-PROJECT-X"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Code
-                </a>
-                <a
-                  className={
-                    styles.portfolio__services__card__down__links__link
-                  }
-                  href={url4}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Webside
-                </a>
-              </div>
-            </div>
+      <div className={styles.master} id="portfolio">
+        <div className={styles.master__portfolio}>
+          <div>
+            <p className={styles.master__portfolio__title}>Portfolio</p>
           </div>
-
-          {/*  CARD 2 */}
-
-          <div className={styles.portfolio__services__card}>
-            <div className={styles.portfolio__services__card__head}>
-              <a
-                type="button"
-                href={url4}
-                target="_blank"
-                rel="noopener noreferrer"
+          <div className={styles.master__portfolio__box}>
+            <div className={styles.master__portfolio__box__cards}>
+              {/* card1 */}
+              <div
+                className={styles.master__portfolio__box__cards__card__card1}
               >
-                <img
-                  className={styles.portfolio__services__card__head}
-                  src={img3}
-                />
-              </a>
-            </div>
-            <div className={styles.portfolio__services__card__down}>
-              <h3 className={styles.portfolio__services__card__down__title}>
-                Stars Wars block API.
-              </h3>
-              <div className={styles.portfolio__services__card__down__tecno}>
-                <h5
+                <div
                   className={
-                    styles.portfolio__services__card__down__tecno__code
+                    styles.master__portfolio__box__cards__card__container
                   }
                 >
-                  JavaScript
-                </h5>
-                <h5
+                  <img
+                    className={
+                      styles.master__portfolio__box__cards__card__container__img
+                    }
+                    src={img1}
+                    alt="las vegas"
+                  />
+                </div>
+                <div
                   className={
-                    styles.portfolio__services__card__down__tecno__code
+                    styles.master__portfolio__box__cards__card__container__details
                   }
                 >
-                  React
-                </h5>
-                <h5
-                  className={
-                    styles.portfolio__services__card__down__tecno__code
-                  }
-                >
-                  CSS
-                </h5>
+                  <h3
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__title
+                    }
+                  >
+                    Las Vegas
+                  </h3>
+                  <p
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__text
+                    }
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Praesentium dignissimos, minus aperiam adipisci
+                    exercitationem.
+                  </p>
+                </div>
               </div>
-              <div className={styles.portfolio__services__card__down__links}>
-                <a
+
+              {/* card2 */}
+
+              <div
+                className={styles.master__portfolio__box__cards__card__card1}
+              >
+                <div
                   className={
-                    styles.portfolio__services__card__down__links__link
+                    styles.master__portfolio__box__cards__card__container
                   }
-                  href="https://github.com/George-Lion/Starwars-Blog-Reading-List"
-                  target="_blank"
-                  rel="noopener noreferrer"
                 >
-                  Code
-                </a>
-                <a
+                  <img
+                    className={
+                      styles.master__portfolio__box__cards__card__container__img
+                    }
+                    src={img3}
+                    alt="las vegas"
+                  />
+                </div>
+                <div
                   className={
-                    styles.portfolio__services__card__down__links__link
+                    styles.master__portfolio__box__cards__card__container__details
                   }
-                  href={url4}
-                  target="_blank"
-                  rel="noopener noreferrer"
                 >
-                  Webside
-                </a>
+                  <h3
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__title
+                    }
+                  >
+                    Las Vegas
+                  </h3>
+                  <p
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__text
+                    }
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Praesentium dignissimos, minus aperiam adipisci
+                    exercitationem.
+                  </p>
+                </div>
+              </div>
+
+              {/* card3 */}
+
+              <div
+                className={styles.master__portfolio__box__cards__card__card1}
+              >
+                <div
+                  className={
+                    styles.master__portfolio__box__cards__card__container
+                  }
+                >
+                  <img
+                    className={
+                      styles.master__portfolio__box__cards__card__container__img
+                    }
+                    src={img1}
+                    alt="las vegas"
+                  />
+                </div>
+                <div
+                  className={
+                    styles.master__portfolio__box__cards__card__container__details
+                  }
+                >
+                  <h3
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__title
+                    }
+                  >
+                    Las Vegas
+                  </h3>
+                  <p
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__text
+                    }
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Praesentium dignissimos, minus aperiam adipisci
+                    exercitationem.
+                  </p>
+                </div>
+              </div>
+
+              {/* card4 */}
+
+              <div
+                className={styles.master__portfolio__box__cards__card__card1}
+              >
+                <div
+                  className={
+                    styles.master__portfolio__box__cards__card__container
+                  }
+                >
+                  <img
+                    className={
+                      styles.master__portfolio__box__cards__card__container__img
+                    }
+                    src={img1}
+                    alt="las vegas"
+                  />
+                </div>
+                <div
+                  className={
+                    styles.master__portfolio__box__cards__card__container__details
+                  }
+                >
+                  <h3
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__title
+                    }
+                  >
+                    Las Vegas
+                  </h3>
+                  <p
+                    className={
+                      styles.master__portfolio__box__cards__card__container__details__text
+                    }
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Praesentium dignissimos, minus aperiam adipisci
+                    exercitationem.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-
-          {/*  CARD 3 */}
-
-          <div className={styles.portfolio__services__card}>
-            <div className={styles.head}></div>
           </div>
         </div>
       </div>
