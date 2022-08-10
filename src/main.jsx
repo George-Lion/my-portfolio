df5f4bd98941c7ca483d4a3840ff5944b790cea2
Jorge Leon <jorgeleonpicture@gmail.com>
2022-08-10 18:56:38 +0200

    create file  main.module.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
+import "./main.module.scss";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import global_es from "./translations/es/global.json";
